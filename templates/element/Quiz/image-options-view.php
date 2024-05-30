800ed61f83b2d1d20b8479b41141d01d61956cce
akram mohammed azizi <akrammed>
2024-05-30 08:41:50 -0700

    building design for image option quiz

--- templates/element/Quiz/image-options-view.php
+++ templates/element/Quiz/image-options-view.php
@@ -14,18 +14,21 @@
 
 <div class="body">
 
-    <section id="section-1" style="margin-top: -8%;">
+    <section id="section-1">
 
         <div class="container">
-
+            <div class="row">
+                <h1 class="title quiz-title-2">
+                    Choose the correct answer</h1>
+            </div>
             <div class="row">
                 <div class="content">
-                    <div class="conversation">
+                    <div class="conversation" style="margin-left: 10%;">
                         <div class="avatar-container">
-                            <?= $this->Html->image('avatar5.png', ['class' => 'avatar-img']) ?>
+                            <?php echo $this->element('icons/monkey'); ?>
                         </div>
                         <div class="speech-container">
-                            <h2><?= $questions[0]['question'] ?> ?</h2>
+                            <h2 class="question-cat"><?= $questions[0]['question'] ?> </h2>
                         </div>
                     </div>
 
@@ -39,9 +42,11 @@
 foreach ($options as $key => $value) {
     if ($value->is_correct == 1) {
         $correctImg = $i;
+        $correct = 'Option-'.$key;
     }
     echo '<div class="col-sm-3 card">'; 
-    echo  $this->Html->image('/img/uploads/picture/'.$value->qoption, ['alt' => 'Option ' . $key,'id'=>'Option-'.$key,'style'=>'height: 100%;', 'class'=>'Option-'.$key.' image-view clickable']);
+    echo  $this->Html->image('/img/uploads/picture/'.$value->qoption, ['alt' => 'Option ' . $key,'id'=>'Option-'.$key,'style'=>'height: 86%;
+    margin-top: 6%;', 'class'=>'Option-'.$key.' image-view clickable img-custom']);
     echo '</div>'; 
      $i++;
 };
@@ -59,7 +64,9 @@ foreach ($options as $key => $value) {
 
 
                 </div>
+
             </div>
+        </div>
 
     </section>
 
@@ -72,19 +79,21 @@ foreach ($options as $key => $value) {
         <div class="row">
             <div class="col-6">
                 <div class="wrong-alert">
-                    <?= $this->Html->image('wrong.svg', ['alt' => 'textalternatif']) ?>
-                    <div id="correct-answers"></div>
+                    <?php echo $this->element('icons/wrong'); ?>
+                    <h3 class="correct-title correct-icon-btn">
+                        Oops, that wasn’t it!</h3>
                 </div>
                 <div class="correct-alert">
-                    <?= $this->Html->image('correct.svg', ['alt' => 'textalternatif']) ?>
-                    <img src="correct.svg" alt="">
-                    <h3 style="margin-left: 6%;" class="correct-title">Good Job !</h3>
+                    <?php echo $this->element('icons/correct'); ?>
+                    <h3 class="correct-title correct-icon-btn">
+                        That's correct ! <br> <span style="font-size: 11px;font-weight: 300;">Well Done !</span></h3>
                 </div>
 
-                <button id="skip" class="btn-skip">Skip</button>
+                <button id="skip" class="btn-skip skip-event">Skip</button>
             </div>
 
-            <div class="col-6"><button id="next" class="btn-next next">Submit</button></div>
+            <div class="col-6"><button id="check" class="btn-next next">Check</button><button id="continue"
+                    class="btn-next next">continue</button></div>
         </div>
     </div>
 </div>
@@ -94,40 +103,53 @@ foreach ($options as $key => $value) {
 
 <script>
 $(document).ready(function(event) {
+    $("#draggable").draggable();
     $(".wrong-alert").hide();
     $(".correct-alert").hide();
+    $('#continue').hide();
+    var selectedOptions = []; // Variable to store selected options as sentences
+    $('#check').prop('disabled', true).css('opacity', '0.2');
+
     var correctA;
     var selected;
     $(document).on('click', '[class^=Option-]', function() {
+        $('#check').prop('disabled', false).css('opacity', '1');
         $('[id^=Option-]').css('border', 'none');
         selected = $(this);
+        console.log(selected);
         var imageId = $(this).attr('id').split('-')[1];
         $(this).css('border', '9px solid greenyellow');
         correctA = parseInt(imageId);
     });
-    $('#next').on('click', function(e) {
+
+    $('#check').on('click', function(e) {
         e.preventDefault();
-
-
         var correctAnswer = <?php echo json_encode($correctImg); ?>;
+        var correct = <?php echo json_encode($correct); ?>;
         if (correctAnswer === correctA) {
+
             $('#skip').hide();
             $(".correct-alert").show();
             $(".wrong-alert").hide();
             $('#onehundred').prop('checked', true);
-            $(this).css('background-color', 'rgb(147, 211, 51, 1)');
-
+            $('.footer').css('background-color', '#E1FCDE');
+            $('#check').hide();
+            $('#continue').show();
         } else {
             selected.css('border', '9px solid #dc3545');
+            $('#' + correct).css('border', '9px solid greenyellow');
+            $('#check').hide();
+            $('#continue').show();
+            $('#continue').css('color', '#B1000F');
+            $('#continue').css('border-color', '#B1000F');
             $('#skip').hide();
             $(".correct-alert").hide();
             $(".wrong-alert").show();
             $('#five').prop('checked', true);
-            $('.footer').css('background-color', 'rgb(32 47 54)')
+            $('.footer').css('background-color', '#FFD4D8')
             $(this).css('background-color', 'rgb(216,72,72,1)');
-            $('#correct-answers').html(
-                '<h3 style="margin-left: 6%;" class="wrong-title">That was not the correct picture !</h3>'
-            );
+            $(".wrong-alert").show();
+            $('.wrong-answer-2').text(correctAnswer.sort());
         }
 
     });
@@ -140,343 +162,32 @@ $(document).ready(function(event) {
 });
 </script>
 
+<?php echo $this->element('Quiz/quiz-style'); ?>
 <style>
-.sortable-list {
-    width: 425px;
-
-    border-radius: 7px;
-    padding: 0px 20px 0px;
-    box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
-}
-
-.sortable-list .item {
-    list-style: none;
-    display: flex;
-    cursor: move;
-    background-color: rgb(55, 70, 79, 1);
-
-    align-items: center;
-    border-radius: 5px;
-    padding: 10px 13px;
-    margin-bottom: 11px;
-    justify-content: space-between;
-}
-
-.item .details {
-    display: flex;
-    align-items: center;
-}
-
-.item .details img {
-    height: 43px;
-    width: 43px;
-    pointer-events: none;
-    margin-right: 12px;
-    object-fit: cover;
-    border-radius: 50%;
-}
-
-.item .details span {
-    font-size: 1.13rem;
-}
-
-.item i {
-    color: black;
-    font-size: 1.13rem;
-}
-
-.item.dragging {
-    opacity: 0.6;
-}
-
-.item.dragging :where(.details, i) {
-    opacity: 0;
-}
-
-body {
-    background-color: rgb(19, 31, 36)
-}
-
-.container-loading {
-    margin: 60px auto;
-    width: 400px;
-    text-align: center;
-    margin-top: 1%;
-}
-
-.container-loading .progress {
-    margin: 0 auto;
-    width: 400px;
-}
-
-.progress {
-    padding: 4px;
-    background: rgba(0, 0, 0, 0.25);
-    border-radius: 6px;
-    -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
-    box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08);
-}
-
-.progress-bar {
-    height: 16px;
-    border-radius: 4px;
-    background-image: -webkit-linear-gradient(top, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
-    background-image: -moz-linear-gradient(top, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
-    background-image: -o-linear-gradient(top, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
-    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.05));
-    -webkit-transition: 0.4s linear;
-    -moz-transition: 0.4s linear;
-    -o-transition: 0.4s linear;
-    transition: 0.4s linear;
-    -webkit-transition-property: width, background-color;
-    -moz-transition-property: width, background-color;
-    -o-transition-property: width, background-color;
-    transition-property: width, background-color;
-    -webkit-box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25), inset 0 1px rgba(255, 255, 255, 0.1);
-    box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25), inset 0 1px rgba(255, 255, 255, 0.1);
+.speech-container h2 {
+    margin: 0;
+    margin-left: -15% !important;
 }
 
 
-#five:checked~.progress>.progress-bar {
-    width: 5%;
-    background-color: #86e01e;
-}
-
-#twentyfive:checked~.progress>.progress-bar {
-    width: 25%;
-    background-color: #86e01e;
-}
-
-#fifty:checked~.progress>.progress-bar {
-    width: 50%;
-    background-color: #86e01e;
-}
-
-#seventyfive:checked~.progress>.progress-bar {
-    width: 75%;
-    background-color: #86e01e;
-}
-
-#onehundred:checked~.progress>.progress-bar {
-    width: 100%;
-    background-color: #86e01e;
-}
-
-.radio {
-    display: none;
-}
-
-.label {
-    display: inline-block;
-    margin: 0 5px 20px;
-    padding: 3px 8px;
-    color: #aaa;
-    text-shadow: 0 1px black;
-    border-radius: 3px;
-    cursor: pointer;
-}
-
-.radio:checked+.label {
-    color: white;
-    background: rgba(0, 0, 0, 0.25);
-}
-
-.footer {
-    border-top: 1px solid white;
+@media only screen and (max-width: 768px) {
+    .options {
+        margin-top: 4%;
+        margin-left: 1% !important;
+    }
+    .img-custom{
+        height: 86% !important;
+    }
+    .footer {
+    height: 70px;
+    border-top: 3px solid #ECEFF4;
     padding: 20px 0;
-    position: relative;
+    position: revert !important;
     bottom: 0;
     width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
-
-.container {
-    width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-}
-
-.btn-skip,
-.btn-next {
-    padding: 10px 20px;
-    background-color: rgb(147, 211, 51, 1);
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    width: 113px;
-    cursor: pointer;
-    margin-top: 4%;
-}
-
-
-.btn-skip {
-    background-color: transparent;
-    border: 2px solid rgb(55, 70, 79, 1);
-    margin-right: auto;
-}
-
-.btn-next {
-    display: block;
-    margin-left: auto;
-}
-
-.line {
-    color: white;
-}
-
-.title {
-    color: white;
-    text-align: center;
-}
-
-.content {
-    color: white;
-    text-align: center;
-    border-bottom: 1px solid white;
-}
-
-.content {
-    text-align: center;
-    /* Center align the content */
-}
-
-.conversation {
-    display: flex;
-    align-items: center;
-    /* Align items vertically */
-}
-
-.avatar-container {
-    margin-right: 20px;
-    /* Adjust spacing between avatar and speech */
-}
-
-.avatar-img {
-    width: 100px;
-    /* Adjust size as needed */
-    height: auto;
-    /* For circular avatars */
-}
-
-.speech-container {
-    background-color: rgb(55, 70, 79, 1);
-    border-radius: 10px;
-    color: ;
-    padding: 10px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    /* Optional: Add a shadow */
-}
-
-.card {
-    background-color: rgb(55, 70, 79, 1);
-    border-radius: 10px;
-    margin-left: o.5%;
-    height: 243px;
-    padding: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    margin-bottom: 2%;
-    cursor: grab;
-}
-
-/* Style the speech text */
-.speech-container h2 {
-    margin: 0;
-    /* Remove default margin */
-}
-
-.answers {
-    border-bottom: 1px solid white;
-    margin-top: 5%;
-    color: white;
-
-}
-
-.options {
-    margin-top: 4%;
-}
-
-.answers ol,
-.options ol {
-    list-style-type: none;
-    /* Remove default list styles */
-    padding: 0;
-    /* Remove default padding */
-    margin: 0;
-    /* Remove default margin */
-    display: flex;
-    /* Use flexbox */
-}
-
-.answers li,
-.options li {
-    margin-right: 10px;
-    /* Adjust spacing between list items */
-}
-
-/* Optional: Adjust styling of list items */
-.answers li,
-.options li {
-    background-color: rgb(55, 70, 79, 1);
-    border-radius: 10px;
-    padding: 10px;
-    color: white;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-}
-
-.wrong-alert {
-    display: flex;
-    align-items: center;
-    color: rgb(216 72 72);
-    margin-left: 5%;
-}
-
-.correct-alert {
-    display: flex;
-    align-items: center;
-    color: rgb(121, 185, 51, 1);
-    margin-left: 5%;
-}
-
-.correct-alert img,
-.wrong-alert img {
-    display: block;
-    margin: 8px 0 0 10px;
-}
-
-.alert {
-    background: rgb(19, 31, 36, 1);
-    border-radius: 98px;
-    display: block;
-    float: left;
-    height: 80px;
-    width: 80px;
-}
-
-
-
-@media (max-width: 450px) {
-    .card {
-        background-color: rgb(55, 70, 79, 1);
-        border-radius: 10px;
-        margin-left: o .5%;
-        height: 344px;
-        padding: 8px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-        margin-bottom: 2%;
-        cursor: grab;
-        width: 77%;
-        margin-left: 14%;
-    }
-    .container-loading .progress {
-    margin: 0 auto;
-    width: 334px;
-    margin-left: 5%;
-    margin-top: 5%;
-    margin-bottom: -13%;
-
-
 }
 </style>
