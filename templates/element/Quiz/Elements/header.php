b55b24d6413f572ba95d1c9afde0e41c80008d9c
akram mohammed azizi <akrammed>
2024-06-05 12:32:36 -0700

    adding match view quiz part 1

---
+++ templates/element/Quiz/Elements/header.php
@@ -0,0 +1,13 @@
+<div class="header">
+    <div class="container-loading ">
+        <input type="radio" class="radio" name="progress" value="five" id="five" checked>
+        <input type="radio" class="radio" name="progress" value="twentyfive" id="twentyfive">
+        <input type="radio" class="radio" name="progress" value="fifty" id="fifty">
+        <input type="radio" class="radio" name="progress" value="seventyfive" id="seventyfive">
+        <input type="radio" class="radio" name="progress" value="onehundred" id="onehundred">
+
+        <div class="progress">
+            <div class="progress-bar"></div>
+        </div>
+    </div>
+</div>
