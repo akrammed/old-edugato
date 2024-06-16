38fa2a5155e64815eb9f003e03651e9d13811a85
RMRdev28 <merouane.mezouari@etu.usthb.dz>
2024-06-16 23:40:31 +0100

    Short page part 1

---
+++ templates/Shorts/watch.php
@@ -0,0 +1,154 @@
+
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+<!-- Side bar -->
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme custom-aside-width">
+
+
+    <div class="app-brand demo">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row" style="margin-left: -8%;">
+                    <div class="col-sm-12">
+                        <h6 class="cours-title-aside"> <a
+                                href="http://localhost:8765/"><?php echo $this->element('icons/return'); ?><span>&nbsp;&nbsp;&nbsp;&nbsp;dfdfdfd</span></a>
+                        </h6>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+
+
+
+
+        <li class="menu-item custom-menu-item active parent-lesson-element">
+            <a href="javascript:void(0);" class="menu-link  lesson-element-style">
+                <?php echo $this->element('icons/home'); ?>
+  
+                    <span class="activities-count" style="margin-left: 4%;"> Home</span>
+                
+
+            </a>
+            <a href="javascript:void(0);" class="menu-link  lesson-element-style">
+                <?php echo $this->element('icons/direction right'); ?>
+             
+                    <span class="activities-count" style="margin-left: 4%;">Learning Path</span>
+
+
+            </a>
+            <a href="javascript:void(0);" class="menu-link  lesson-element-style">
+                <?php echo $this->element('icons/graduated'); ?>
+             
+                    <span class="activities-count" style="margin-left: 4%;">Courses</span>
+ 
+
+            </a>
+            <a href="javascript:void(0);" class="menu-link  lesson-element-style">
+                <?php echo $this->element('icons/profile'); ?>
+              
+                    <span class="activities-count" style="margin-left: 4%;">Profile</span>
+                
+
+            </a>
+
+            <ul class="menu-sub">
+     
+                        $countC++; ?>
+                <li class="menu-item  ">
+                    <a style="height: 48px !important;" href="#" id="1"
+                        class="menu-link sub-menu-link chapter-element chap">
+
+            
+                        <span class="icon-video" style="margin-left: -13%;">
+                            <?php echo $this->element('icons/video'); ?></span>
+
+
+                        <span class="course-title-style"> chapter 1 </span>
+                    </a>
+
+                </li>
+
+
+            </ul>
+        </li>
+
+
+    </ul>
+</aside>
+<div class="layout-page">
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme custom-navbar-style custom-header"
+
+                id="layout-navbar">
+                <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+                    <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                        <i class="bx bx-menu bx-sm"></i>
+                    </a>
+                </div>
+
+                <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                    <div class="navbar-nav align-items-center">
+                        <div class="nav-item d-flex align-items-center">
+                            <span class="navbar-title">Hello </span>
+                        </div>
+                    </div>
+
+
+                    <ul class="navbar-nav flex-row align-items-center ms-auto">
+
+                        <li class="nav-item lh-1 me-3 custom-display">
+                            <?php echo $this->element('icons/score'); ?>
+
+                        </li>
+                        <li class="nav-item lh-1 me-3 custom-display">
+                            <?php echo $this->element('icons/points'); ?>
+
+                        </li>
+
+                        <li class="nav-item lh-1 me-3 custom-display">
+                            <?php echo $this->element('icons/notification'); ?>
+
+                        </li>
+                        <!-- User -->
+                        <li class="nav-item navbar-dropdown dropdown-user dropdown custom-margin">
+                            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
+                                data-bs-toggle="dropdown">
+                                <div class="avatar avatar-online">
+                                    <?= $this->Html->image('profile.png', ['class' => 'w-px-40 h-auto rounded-circle']) ?>
+                                </div>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+
+                                <li>
+                                    <a class="dropdown-item" href="https://www.edugato.net/profile" style="color:black">
+                                        <i class="bx bx-user me-2"></i>
+                                        <span class="align-middle">My Profile</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <div class="dropdown-divider"></div>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="https://www.edugato.net/logout" style="color:black">
+                                        <i class="bx bx-power-off me-2"></i>
+                                        <span class="align-middle">Log Out</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+                        </div>
+                        </div>
+                        </div>
---
+++ templates/element/icons/home.php
@@ -0,0 +1,4 @@
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.56543 13.4463H12.4112" stroke="#1A212B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M2 11.0371C2 6.53235 2.51241 6.84675 5.27061 4.39475C6.47737 3.46355 8.35509 1.66675 9.97662 1.66675C11.5973 1.66675 13.5126 3.45475 14.7302 4.39475C17.4884 6.84675 18 6.53235 18 11.0371C18 17.6667 16.3651 17.6667 9.99999 17.6667C3.63489 17.6667 2 17.6667 2 11.0371Z" stroke="#1A212B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
