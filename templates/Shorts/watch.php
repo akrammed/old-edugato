f2ab1b3786a4d3164005e72a21c902251522de77
RMRdev28 <merouane.mezouari@etu.usthb.dz>
2024-06-20 23:25:48 +0100

    Refactor  js for shorts

--- templates/Shorts/watch.php
+++ templates/Shorts/watch.php
@@ -1,30 +1,26 @@
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+        <!-- Side bar -->
+        <?php echo $this->element('aside'); ?>
+        <!-- Container -->
+        <div class="layout-page">
+            <!-- Navbar I-->
+            <?php echo $this->element('nav'); ?>
 
-    <div class="layout-wrapper layout-content-navbar">
-        <div class="layout-container">
-            <!-- Side bar -->
-            <?php echo $this->element('aside'); ?>
-            <!-- Container -->
-            <div class="layout-page">
-                <!-- Navbar -->
-                <?php echo $this->element('nav'); ?>
-
-                <!-- Content -->
-                <div class="content-wrapper">
-                    <div class="container-xxl flex-grow-1 mt-3 content-container" id="scContent">
-                        <div class="row">
-                            <div class="col-md-10 w" style="min-height:619px!important; border-radius: 16px ">
-                                <div class="card mb-3">
-                                    <div class="row ">
-                                        <div class="col-md-4" id="scrolledDiv">
-                                        </div>
-                                        <div class="col-md-8" id="main">
-                                            <?php foreach ($shorts as $short): ?>
-                                            <section class="hidden-section sec" id="section-<?= $short->id ?>">
+            <!-- Content -->
+            <div class="content-wrapper">
+                <div class="container-xxl flex-grow-1 mt-3 content-container" id="scContent">
+                    <div class="row">
+                        <div class="col-md-10 w" style="min-height:619px!important; border-radius: 16px ">
+                            <div class="card mb-3">
+                                <div class="row ">
+                                    <div class="col-md-4" id="scrolledDiv">
+                                    </div>
+                                    <div class="col-md-8" id="main">
+                                            <section class="sec" id="section">
                                                 <div class="card-body h-100" id="take-quiz-2">
-                                                    <div
-                                                        class="conversation h-100 w-100 d-flex justify-content-center align-items-center">
-                                                        <div
-                                                            class="avatar-container d-flex flex-row align-items-center">
+                                                    <div class="conversation h-100 w-100 d-flex justify-content-center align-items-center">
+                                                        <div class="avatar-container d-flex flex-row align-items-center">
                                                             <?php echo $this->element('icons/avatar'); ?>
                                                             <?php echo $this->element('icons/talikng-bubbls'); ?>
 
@@ -33,26 +29,24 @@
                                                     </div>
                                                 </div>
                                             </section>
-                                            <?php endforeach; ?>
-                                        </div>
-
                                     </div>
+
                                 </div>
                             </div>
-                            <div class="col-sm-2">
-                                <div class="card mb-3" style="min-height: 499px !important; border-radius: 16px; ">
-                                    <div class="row g-0">
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="card mb-3" style="min-height: 499px !important; border-radius: 16px; ">
+                                <div class="row g-0">
 
-                                    </div>
                                 </div>
-                                <div class="d-block">
-                                    <button class="mb-1" style="background-color: #F6F8FB; border:none;">
-                                        <?php echo $this->element('icons/arrow-top'); ?>
-                                    </button><br>
-                                    <button style="background-color: #F6F8FB; border:none;">
-                                        <?php echo $this->element('icons/arrow-down'); ?>
-                                    </button>
-                                </div>
+                            </div>
+                            <div class="d-block">
+                                <button id="btnScrollUp" class="mb-1" style="background-color: #F6F8FB; border:none;">
+                                    <?php echo $this->element('icons/arrow-top'); ?>
+                                </button><br>
+                                <button id="btnScrollDown" style="background-color: #F6F8FB; border:none;">
+                                    <?php echo $this->element('icons/arrow-down'); ?>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -60,46 +54,56 @@
             </div>
         </div>
     </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
     $(document).ready(function() {
-        var firstShortId = $('section:first').attr('id').split('-')[1];
+        const shortsArray = <?= json_encode($shortsList) ?>; 
+        console.log(shortsArray);
+        var i = 0;
+        var firstShortId = shortsArray[i];
         var ajaxSent = false;
-        var shortId = firstShortId;
-
-        console.log("First Short ID: " + firstShortId);
+        var lastScrollTop = 0;
+        
         debounceLog(firstShortId);
-        $(`#section-${firstShortId}`).removeClass('hidden-section');
-
-        $('#scContent').on('scroll', function() {
-            var viewportTop = $(this).scrollTop();
-            var viewportBottom = viewportTop + $(this).height();
-
-            $('section').each(function() {
-                var sectionTop = $(this).offset().top - $('#scContent').offset().top;
-                var sectionBottom = sectionTop + $(this).outerHeight();
-
-                if (sectionTop < viewportBottom && sectionBottom > viewportTop) {
-                    shortId = $(this).attr('id').split('-')[1];
-                    return false;
-                }
-            });
-
-            if (shortId && !ajaxSent) {
-                $('section').addClass('hidden-section');
-                $(`#section-${shortId}`).removeClass('hidden-section');
-                console.log("Short ID: " + shortId);
-                debounceLog(shortId);
+        verifyI(i);
+        $('#scrolledDiv').on('scroll', function() {
+            var currentScrollTop = $(this).scrollTop();
+            var scrollDirection = currentScrollTop > lastScrollTop ? "down" : "up";
+            if(scrollDirection === "down" && !ajaxSent) {
+             scroll("down");
+            } else if(scrollDirection === "up" && !ajaxSent) {
+            scroll("up");
             }
+            lastScrollTop = currentScrollTop;
         });
-
-        $('#btn').click(function() {
-            $('#scContent').scroll();
+        $('#btnScrollUp').click(function() {
+           scroll("up");
+        });
+        $('#btnScrollDown').click(function() {
+         scroll("down");
         });
 
         var timeout;
-
+        function scroll(type){
+            if(type === "down"){
+                i++;
+                if(i < shortsArray.length) {
+                    shortId = shortsArray[i];
+                    debounceLog(shortId);
+                    verifyI(i);
+                }
+            }else{
+                i--;
+                if(i < shortsArray.length) {
+                    shortId = shortsArray[i];
+                    debounceLog(shortId);
+                    verifyI(i);
+                }
+            }
+  
+        }
         function debounceLog(shortId) {
             clearTimeout(timeout);
             timeout = setTimeout(function() {
@@ -151,45 +155,58 @@
                 });
             }, 150);
         }
+
+        function verifyI(i){
+            if(i == 0){
+                $('#btnScrollUp').hide();
+            } else {
+                $('#btnScrollUp').show();
+            }
+            if(i == shortsArray.length - 1){
+                $('#btnScrollDown').hide();
+            } else {
+                $('#btnScrollDown').show();
+            }
+        }
     });
-    </script>
-    <style>
-.hidden-section {
-    display: none;
-}
+</script>
+<style>
+    .hidden-section {
+        display: none;
+    }
 
-#scrolledDiv {
-    scroll-behavior: smooth;
-}
+    #scrolledDiv {
+        scroll-behavior: smooth;
+    }
 
-#scContent {
-    overflow-y: scroll;
-}
+    #scContent {
+        overflow-y: scroll;
+    }
 
-.sec {
-    height: 619px;
-}
+    .sec {
+        height: 619px;
+    }
 
-.avatar-question {
-    display: flex;
-    width: 245px;
-    padding: 16px 20px;
-    align-items: center;
-    border-radius: 60px;
-    border: 1px solid var(--Outline-Container, rgba(154, 168, 188, 0.20));
-    background: var(--Background-Primary, #FFF);
-    box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.04);
-    color: black !important;
-    font-family: Poppins;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 18px;
-}
+    .avatar-question {
+        display: flex;
+        width: 245px;
+        padding: 16px 20px;
+        align-items: center;
+        border-radius: 60px;
+        border: 1px solid var(--Outline-Container, rgba(154, 168, 188, 0.20));
+        background: var(--Background-Primary, #FFF);
+        box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.04);
+        color: black !important;
+        font-family: Poppins;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 18px;
+    }
 
-.custom-video-container {
-    padding: 0%;
-    padding-left: 0%;
-    height: 620px;
-}
+    .custom-video-container {
+        padding: 0%;
+        padding-left: 0%;
+        height: 620px;
+    }
 </style>
