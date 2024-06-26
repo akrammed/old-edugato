211717402583e4f46379d34aa4e310d4eafe635a
douaabthr <douaaboutehra19@gmail.com>
2024-06-26 18:28:32 +0100

    system quiz types part one

--- templates/Quizs/add.php
+++ templates/Quizs/add.php
@@ -7,7 +7,7 @@
  */
 ?>
 
-<div class="container" id="quiz-type-section-1">
+<div class="container" id="quiz-type-section">
     <h3 class="pb-1 mb-4 mt-4 text-dark fw-normal main-title">Add a Quiz Type</h3>
     <div class="row">
         <div class="col-md-6 col-lg-4 mb-4">
@@ -53,8 +53,74 @@
         </div>
     </div>
 </div>
+<div id="quiz-type-creation-section" style="display: none;">
+    <div id="quiz-creation-container">
+        <button class="upload-short-video" id="return">Retour</button>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.quiz-icon').on('click', function(e) {
+            e.preventDefault();
+            var id = $(this).data('id');
+            $('#quiz-type-section').hide();
+            $('#quiz-type-creation-section').show();
+            var template = '';
+            switch (id) {
+                case 1:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 1.</p>';
+                    break;
+                case 2:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 2.</p>';
+                    break;
+                case 3:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 3.</p>';
+                    break;
+                case 4:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 4.</p>';
+                    break;
+                case 5:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 5.</p>';
+                    break;
+                case 6:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 6.</p>';
+                    break;
+                case 7:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 7.</p>';
+                    break;
+                case 8:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 8.</p>';
+                    break;
+                case 9:
+                    template = '<p class="prg">Vous avez sélectionné l\'option 9.</p>';
+                    break;
+                default:
+                    template = '<p class="prg">Option non reconnue.</p>';
+                    break;
+            }
+            $('.prg').remove();
+            $('#quiz-creation-container').append(template);
 
+        });
+        $('#return').on('click', function(e) {
+            e.preventDefault();
+            $('#quiz-type-creation-section').hide();
+            $('#quiz-type-section').show();
+        });
+    });
+</script>
 <style>
+    .upload-short-video {
+        background-color: #5C17E5;
+        color: white;
+        border: none;
+        width: 108px;
+        height: 39px;
+        border-radius: 16px;
+        margin-left: 6%;
+    }
+
     .quiz-card-type {
         box-shadow: none;
         background-color: #F6F8FB;
@@ -79,5 +145,15 @@
     .main-title {
         font-size: 18px;
         font-weight: 600 !important;
+    }
+    #return {
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+    }
+
+    #quiz-type-creation-section {
+        position: relative;
+        min-height: 200px;
     }
 </style>
